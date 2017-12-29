0100d3c0c277633c741fbf18d515fa72eb8e1f92
weixiaoyue <xiaoyue.wei@zzlgroup.com>
2017-12-29 16:07:33 +0800

    modify

--- environment/dev.env.js
+++ environment/dev.env.js
@@ -1,6 +1,5 @@
 module.exports = {
   URL: {
-    SERVER: JSON.stringify("http://192.168.3.85:9001"),
-    UPLOAD_URL: JSON.stringify("http://192.168.3.85:9001")
+    SERVER: JSON.stringify("http://192.168.3.85:9001")
   }
 }
