5c7c8a2e1e27c6069679e71fb19b12e290b963d4
weixiaoyue <xiaoyue.wei@zzlgroup.com>
2018-01-03 17:11:07 +0800

    role

--- src/pages/system-manage/user-manage.vue
+++ src/pages/system-manage/user-manage.vue
@@ -1,6 +1,34 @@
 <template>
   <section class="page user-manage">
-    UserManage
+    <el-row type="flex">
+      <el-col :span="8" style="flex-basis:250px;border:1px solid #e4e4e4;min-height:595px">
+        <el-tree></el-tree>
+      </el-col>
+      <el-col :span="16">
+        <data-box :data="userDataSet">
+          <template slot="columns">
+            <el-table-column prop="name" label="角色名称">
+            </el-table-column>
+            <el-table-column prop="remark" label="角色描述">
+            </el-table-column>
+            <el-table-column prop="status" label="状态">
+            </el-table-column>
+            <el-table-column prop="date" label="更新时间">
+            </el-table-column>
+            <el-table-column prop="operator" label="操作人">
+            </el-table-column>
+            <el-table-column label="操作" min-width="150">
+              <template slot-scope="scope">
+                <el-button type="text" @click="updateRoleClick(scope.row)">修改</el-button>
+                <el-button type="text" @click="permissionClick(scope.row)">模块权限</el-button>
+                <el-button type="text" @click="checkUserList(scope.row)">用户列表</el-button>
+                <el-button type="text" @click="deleteRole(scope.row)">删除</el-button>
+              </template>
+            </el-table-column>
+          </template>
+        </data-box>
+      </el-col>
+    </el-row>
   </section>
 </template>
 
@@ -10,12 +38,19 @@
   import {
     Layout
   } from "~/core/decorator";
+  import DataForm from "~/components/common/data-form.vue";
+  import DataBox from "~/components/common/data-box.vue";
 
   @Layout('workspace')
   @Component({
-    components: {}
+    components: {
+      DataForm,
+      DataBox
+    }
   })
-  export default class UserManage extends Vue {}
+  export default class UserManage extends Vue {
+    private userDataSet: Array<any> = [];
+  }
 
 </script>
 
