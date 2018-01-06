8655dca6bfd89fe2c2f52e7a0998ccc6982403a1
zhuchentong <2037630@163.com>
2018-01-06 18:34:06 +0800

    add map

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,17 @@ import store from './store'
 import router from './router'
 import bootstrap from '~/core/bootstrap'
 import iView from 'iview'
+import AMap from 'vue-amap';
 
 bootstrap({ store })
 
 Vue.use(iView)
+Vue.use(AMap);
+
+AMap.initAMapApiLoader({
+  key: '0240ff10dd31d192c04ae9e0d4c76561',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor']
+});
 
 Vue.config.productionTip = false
 
