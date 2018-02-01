9054a4c4a16d848bb1bb2a0e76435368a154651b
wangzejie <543118962@qq.com>
2018-02-01 18:14:32 +0800

    update

--- src/pages/finance/initial-payment-manage/payment.vue
+++ src/pages/finance/initial-payment-manage/payment.vue
@@ -2,27 +2,27 @@
 <template>
   <section class="page payment">
     <span class="form-title">付款</span>
-    <i-button type="text">昨日</i-button>
-    <i-button type="text">今日</i-button>
-    <i-button type="text">本周</i-button>
-    <i-button type="text">本月</i-button>
-    <i-button type="text">上月</i-button>
-    <i-button type="text">最近三月</i-button>
-    <i-button type="text">本季度</i-button>
-    <i-button type="text">本年</i-button>
+    <i-button type="text" @click="getTimeSearch(0)">昨日</i-button>
+    <i-button type="text" @click="getTimeSearch(1)">今日</i-button>
+    <i-button type="text" @click="getTimeSearch(2)">本周</i-button>
+    <i-button type="text" @click="getTimeSearch(3)">本月</i-button>
+    <i-button type="text" @click="getTimeSearch(4)">上月</i-button>
+    <i-button type="text" @click="getTimeSearch(5)">最近三月</i-button>
+    <i-button type="text" @click="getTimeSearch(6)">本季度</i-button>
+    <i-button type="text" @click="getTimeSearch(7)">本年</i-button>
     <i-button @click="openSearch" style="color:#265EA2">
       <span v-if="!searchOptions">展开</span>
       <span v-if="searchOptions">收起</span>
       <span>高级搜索</span>
     </i-button>
     <i-row v-if="searchOptions" style="margin:6px;position:relative;right:16px;">
-      <i-input style="display:inline-block;width:18%;margin-left:20px;" placeholder="请录入付款账户名查询"></i-input>
+      <i-input style="display:inline-block;width:18%;margin-left:20px;" v-model="approvalModel.refundName" placeholder="请录入付款账户名查询"></i-input>
       <span style="margin-left:10px">日期：</span>
-      <i-date-picker style="display:inline-block;width:10%"></i-date-picker>~
-      <i-date-picker style="display:inline-block;width:10%"></i-date-picker>
-      <i-button style="margin-left:10px" class="blueButton">搜索</i-button>
+      <i-date-picker type="date" style="display:inline-block;width:10%" v-model="approvalModel.startTime"></i-date-picker>~
+      <i-date-picker type="date" style="display:inline-block;width:10%" v-model="approvalModel.endTime"></i-date-picker>
+      <i-button style="margin-left:10px" @click="getOrderQuery" class="blueButton">搜索</i-button>
     </i-row>
-    <data-box :columns="columns1" :data="data1"></data-box>
+    <data-box :columns="columns1" :data="paymentData"></data-box>
 
     <template>
       <i-modal v-model="openColumnsConfig" title="列配置" @on-ok="confirm">
@@ -58,8 +58,17 @@
     Tooltip
   } from 'iview'
   import {
+    FilterService
+  } from "~/utils/filter.service";
+  import {
     Dependencies
   } from "~/core/decorator";
+  import {
+    RefundApplicationService
+  } from "~/services/manage-service/refund.application.service";
+  import {
+    PageService
+  } from "~/utils/page.service";
   import {
     Layout
   } from "~/core/decorator";
@@ -73,14 +82,24 @@
     }
   })
   export default class Payment extends Page {
+    @Dependencies(RefundApplicationService) private refundApplicationService: RefundApplicationService;
+    @Dependencies(PageService) private pageService: PageService;
     private columns1: any;
-    private data1: Array < Object > = [];
+    private paymentData: Array < Object > = [];
     private columns2: any;
     private data2: Array < Object > = [];
     private searchOptions: Boolean = false;
+    private refundName: String = '';
     private openColumnsConfig: Boolean = false;
     private confirmGatherModal: Boolean = false;
-
+    private startTime: any;
+    private endTime: any;
+    private approvalModel: any = {
+      refundName: '',
+      timeSearch: '',
+      startTime: '',
+      endTime: ''
+    }
     openSearch() {
       this.searchOptions = !this.searchOptions;
     }
@@ -88,7 +107,33 @@
       this.$Message.success('保存草稿成功！')
       this.confirmGatherModal = false
     }
+    getOrderQuery() {
+      this.approvalModel.startTime = FilterService.dateFormat(
+        this.approvalModel.startTime,
+        "yyyy-MM-dd"
+      );
+      this.approvalModel.endTime = FilterService.dateFormat(
+        this.approvalModel.endTime,
+        "yyyy-MM-dd"
+      );
+      this.refundApplicationService.getApprovalRecord(this.approvalModel, this.pageService).subscribe(val => {
+        this.paymentData = val.object.list
+      })
+    }
+    getTimeSearch(val) {
+      this.approvalModel.startTime = "";
+      this.approvalModel.endTime = "";
+      this.approvalModel.timeSearch = val;
+      this.getOrderQuery();
+      this.approvalModel.timeSearch = "";
+    }
+    getApproval() {
+      this.refundApplicationService.getApprovalRecord(this.approvalModel, this.pageService).subscribe(val => {
+        this.paymentData = val.object.list
+      })
+    }
     created() {
+      this.getApproval()
       this.columns1 = [{
           align: "center",
           type: "index",
@@ -169,47 +214,55 @@
           }
         },
         {
-          title: "处理状态",
-          key: "handleStatus",
-          align: "center"
-        },
-        {
-          align: "center",
-          title: "处理时间",
-          key: "handleTime",
-          width: 180
-        },
-        {
-          align: "center",
-          title: "处理人",
-          key: "handlePerson"
-        },
-        {
-          align: "center",
-          title: "付款类型",
-          key: "gatheringType"
-        },
-        {
-          align: "center",
-          title: "付款总金额",
-          key: "gatheringTotalAmt"
-        },
-        {
-          align: "center",
-          title: "付款账户名",
-          key: "gatheringAccountName"
-        },
-        {
-          align: "center",
-          title: "申请日期",
-          key: "applyDate"
-        },
-        {
-          align: "center",
-          title: "申请人",
-          key: "applyPerson"
+          title: '处理状态',
+          key: 'processStatus',
+          align: 'center'
+        }, {
+          title: '处理时间',
+          key: 'processTime',
+          width: 180,
+          align: 'center',
+          render: (h, {
+            row,
+            column,
+            index
+          }) => {
+            return h('span', FilterService.dateFormat(row.processTime, 'yyyy-MM-dd hh:mm:ss'))
+          }
+        }, {
+          title: '处理人',
+          width: 120,
+          key: 'processName',
+          align: 'center'
+        }, {
+          title: '付款类型',
+          key: 'refundType',
+          align: 'center'
+        }, {
+          title: '付款总金额',
+          key: 'refundTotalAmount',
+          align: 'center'
+        }, {
+          title: '付款账户名',
+          key: 'customerName',
+          align: 'center'
+        }, {
+          title: '申请日期',
+          key: 'operateTime',
+          align: 'center',
+          render: (h, {
+            row,
+            column,
+            index
+          }) => {
+            return h('span', FilterService.dateFormat(row.operateTime, 'yyyy-MM-dd hh:mm:ss'))
+          }
+        }, {
+          title: '制单人',
+          key: 'operator',
+          align: 'center'
         }
-      ];
+      ]
 
       this.data1 = [{
         handleStatus: '未处理',
