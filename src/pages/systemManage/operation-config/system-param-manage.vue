36e523b24550216a110b8f8e2d13ee5c497b2ea2
liujia <284014405@qq.com>
2018-02-05 19:48:16 +0800

    update

--- src/pages/systemManage/operation-config/system-param-manage.vue
+++ src/pages/systemManage/operation-config/system-param-manage.vue
@@ -12,7 +12,7 @@
       </i-select>
       <i-button style="margin-left:10px;" class="blueButton" @click="getSystemParam">搜索</i-button>
     </i-row>
-    <data-box :columns="columns1" :data="systemParamsData"></data-box>
+    <data-box :columns="columns1" :data="systemParamsData" @onPageChange="getSystemParam" :page="pageService"></data-box>
 
     <template>
       <i-modal v-model="editSysParamsModal" title="修改系统参数">
@@ -27,150 +27,171 @@
 </template>
 
 <script lang="ts">
-import Page from '~/core/page';
-import Component from 'vue-class-component';
-import DataBox from '~/components/common/data-box.vue';
-import { PageService } from '~/utils/page.service';
-import { Dependencies } from '~/core/decorator';
-import { OrderService } from '~/services/business-service/order.service';
-import SvgIcon from '~/components/common/svg-icon.vue';
-import { Layout } from '~/core/decorator';
-import ModifySystemParams from '~/components/system-manage/modify-system-params.vue';
-import { SystemParameterService } from '~/services/manage-service/system.parameter.service';
+  import Page from '~/core/page';
+  import Component from 'vue-class-component';
+  import DataBox from '~/components/common/data-box.vue';
+  import {
+    PageService
+  } from '~/utils/page.service';
+  import {
+    Dependencies
+  } from '~/core/decorator';
+  import {
+    OrderService
+  } from '~/services/business-service/order.service';
+  import SvgIcon from '~/components/common/svg-icon.vue';
+  import {
+    Layout
+  } from '~/core/decorator';
+  import ModifySystemParams from '~/components/system-manage/modify-system-params.vue';
+  import {
+    SystemParameterService
+  } from '~/services/manage-service/system.parameter.service';
 
-@Layout('workspace')
-@Component({
-	components: {
-		DataBox,
-		SvgIcon,
-		ModifySystemParams,
-	},
-})
-export default class OrderTransfer extends Page {
-	@Dependencies() private pageService: PageService;
-	@Dependencies(SystemParameterService) private systemParameterService: SystemParameterService;
-	private columns1: any;
-	private columns2: any;
-	private systemParamsData: Array<Object> = [];
-	private customName: String = '';
-	private openColumnsConfig: Boolean = false;
-	private openOneKeyToConnect: Boolean = false;
-	private editSysParamsModal: Boolean = false;
-	private systemParameterModel: any;
-	private checkRadio: String = '';
-	private modifySysParamsModel: any;
-	activated() {}
-	created() {
-		this.getSystemParam();
-		this.systemParameterModel = {
-			paramName: '',
-			paramStatus: '',
-		};
-		this.modifySysParamsModel = {
-			paramCode: '',
-			paramName: '',
-			paramValue: '',
-			paramStatus: '',
-		};
-		this.columns1 = [
-			{
-				align: 'center',
-				type: 'index',
-				title: '序号',
-				width: 60,
-			},
-			{
-				title: '操作',
-				align: 'center',
-				width: 120,
-				render: (h, { row, columns, index }) => {
-					return h(
-						'i-button',
-						{
-							props: {
-								type: 'text',
-							},
-							on: {
-								click: () => {
-									this.modifySysParams(row);
-								},
-							},
-							style: {
-								color: '#265EA2',
-							},
-						},
-						'修改'
-					);
-				},
-			},
-			{
-				title: '参数代码',
-				key: 'paramCode',
-				align: 'center',
-			},
-			{
-				title: '参数名称',
-				key: 'paramName',
-				align: 'center',
-			},
-			{
-				title: '参数值',
-				key: 'paramValue',
-				align: 'center',
-			},
-			{
-				title: '是否启用',
-				key: 'paramStatus',
-				align: 'center',
-				render: (h, { row, columns, index }) => {
-					return h('span', {}, row.paramStatus === 0 ? '启用' : '停用');
-				},
-			},
-			{
-				title: '说明',
-				key: 'information',
-				align: 'center',
-			},
-		];
-	}
-	getOrderInfoByTime() {}
-	oneKeyToConnect() {
-		this.openOneKeyToConnect = true;
-	}
-	/**
-	 * 列配置
-	 */
-	columnsConfig() {
-		this.openColumnsConfig = true;
-	}
-	/**
-	 * 多选
-	 */
-	multipleSelect(selection) {}
-	/**
-	 * 修改按钮
-	 */
-	modifySysParams(row) {
-		this.editSysParamsModal = true;
-		this.modifySysParamsModel = row;
-	}
-	/**
-	 * 获取分页查询系统参数
-	 */
-	getSystemParam() {
-		this.systemParameterService
-			.querySystemParameterPage(this.systemParameterModel, this.pageService)
-			.subscribe(val => {
-				this.systemParamsData = val;
-			});
-	}
-	confirmModifySysParams() {
-		let _modify: any = this.$refs['modify-sys-param'];
-		_modify.confirmModify();
-	}
-	closeBtn() {
-		this.editSysParamsModal = false;
-		this.getSystemParam();
-	}
-}
+  @Layout('workspace')
+  @Component({
+    components: {
+      DataBox,
+      SvgIcon,
+      ModifySystemParams
+    },
+  })
+  export default class OrderTransfer extends Page {
+    @Dependencies() private pageService: PageService;
+    @Dependencies(SystemParameterService) private systemParameterService: SystemParameterService;
+    private columns1: any;
+    private columns2: any;
+    private systemParamsData: Array < Object > = [];
+    private customName: String = '';
+    private openColumnsConfig: Boolean = false;
+    private openOneKeyToConnect: Boolean = false;
+    private editSysParamsModal: Boolean = false;
+    private systemParameterModel: any;
+    private checkRadio: String = '';
+    private modifySysParamsModel: any;
+    activated() {}
+    created() {
+      this.getSystemParam();
+      this.systemParameterModel = {
+        paramName: '',
+        paramStatus: '',
+      };
+      this.modifySysParamsModel = {
+        paramCode: '',
+        paramName: '',
+        paramValue: '',
+        paramStatus: '',
+      };
+      this.columns1 = [{
+          align: 'center',
+          type: 'index',
+          title: '序号',
+          width: 60,
+        },
+        {
+          title: '操作',
+          align: 'center',
+          width: 120,
+          render: (h, {
+            row,
+            columns,
+            index
+          }) => {
+            return h(
+              'i-button', {
+                props: {
+                  type: 'text',
+                },
+                on: {
+                  click: () => {
+                    this.modifySysParams(row);
+                  },
+                },
+                style: {
+                  color: '#265EA2',
+                },
+              },
+              '修改'
+            );
+          },
+        },
+        {
+          title: '参数代码',
+          key: 'paramCode',
+          align: 'center',
+        },
+        {
+          title: '参数名称',
+          key: 'paramName',
+          align: 'center',
+        },
+        {
+          title: '参数值',
+          key: 'paramValue',
+          align: 'center',
+        },
+        {
+          title: '是否启用',
+          key: 'paramStatus',
+          align: 'center',
+          render: (h, {
+            row,
+            columns,
+            index
+          }) => {
+            return h('span', {}, row.paramStatus === 0 ? '启用' : '停用');
+          },
+        },
+        {
+          title: '说明',
+          key: 'information',
+          align: 'center',
+        },
+      ];
+    }
+    getOrderInfoByTime() {}
+    oneKeyToConnect() {
+      this.openOneKeyToConnect = true;
+    }
+    /**
+     * 列配置
+     */
+    columnsConfig() {
+      this.openColumnsConfig = true;
+    }
+    /**
+     * 多选
+     */
+    multipleSelect(selection) {}
+    /**
+     * 修改按钮
+     */
+    modifySysParams(row) {
+      this.editSysParamsModal = true;
+      this.modifySysParamsModel = row;
+    }
+    /**
+     * 获取分页查询系统参数
+     */
+    getSystemParam() {
+      this.systemParameterService
+        .querySystemParameterPage(this.systemParameterModel, this.pageService)
+        .subscribe(val => {
+          this.systemParamsData = val;
+        }, ({
+          msg
+        }) => {
+          this.$Message.error(msg)
+        });
+    }
+    confirmModifySysParams() {
+      let _modify: any = this.$refs['modify-sys-param'];
+      _modify.confirmModify();
+    }
+    closeBtn() {
+      this.editSysParamsModal = false;
+      this.getSystemParam();
+    }
+  }
+
 </script>
