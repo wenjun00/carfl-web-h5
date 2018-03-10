00cf866f9ffa033f848c7a35a6a209b531450b53
liuchen <294007166@qq.com>
2018-03-10 15:15:46 +0800

    修改

--- src/config/server/index.ts
+++ src/config/server/index.ts
@@ -2,3 +2,4 @@ export * from './business-service'; // 运营服务接口配置
 export * from './test-service'; // 运营服务接口配置
 export * from './manage-service'; // 管理服务接口配置
 export * from './file-service'; // 管理文件上传接口配置
+export * from './report-service'; // 管理文件上传接口配置
