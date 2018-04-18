e308d06da4123357f6201e74fde60c684ff64eb5
chenli <1019706609@qq.com>
2018-04-18 15:44:41 +0800

    modify

--- src/pages/base/base-data/prod-package-info.vue
+++ src/pages/base/base-data/prod-package-info.vue
@@ -1,27 +1,27 @@
 <!--产品包管理-->
 <template>
   <section class="page prod-package-info">
-    <i-row style="margin-top:10px;margin-left:10px;">
-      <span style="font-size:18px;font-weight:bold">产品包管理</span>
-      <span style="margin-left:10px;">文件名：</span>
-      <i-input style="width:10%" v-model="productModel.fileName"></i-input>
-      <span style="margin-left:10px;">上传时间：</span>
-      <i-date-picker style="display:inline-block;width:10%;" v-model="productModel.minDate"></i-date-picker>~
-      <i-date-picker style="display:inline-block;width:10%;" v-model="productModel.maxDate"></i-date-picker>
-      <i-button class="blueButton" style="margin-left:10px;" @click="getProductPackage">搜索</i-button>
-      <i-button class="blueButton" style="margin-left:10px;" @click="resetSeach">重置</i-button>
-      <div style="float:right;margin-right:10px;margin-top:10px;">
-        <div style="font-size:16px;cursor:pointer;margin-left:10px;color:#3367A7">
+    <i-row class="data-form">
+      <span class="form-title title">产品包管理</span>
+      <span>文件名：</span>
+      <i-input class="data-form-item" v-model="productModel.fileName"></i-input>
+      <span>上传时间：</span>
+      <i-date-picker class="data-form-item" v-model="productModel.minDate"></i-date-picker>~
+      <i-date-picker class="data-form-item" v-model="productModel.maxDate"></i-date-picker>
+      <i-button class="blueButton" @click="getProductPackage">搜索</i-button>
+      <i-button class="blueButton" @click="resetSeach">重置</i-button>
+      <div class="command">
+        <div class="command-item">
           <svg-icon iconClass="xiazai"></svg-icon>
-          <i-button type="text" style="font-size: 12px;" @click="QuotationTemplatedownload">报价模板下载</i-button>
+          <i-button type="text" @click="QuotationTemplatedownload">报价模板下载</i-button>
         </div>
       </div>
     </i-row>
-    <data-box :id="91" :columns="columns" :data="prdPackageList" @onPageChange="getProductPackage" :page="pageService" ></data-box>
+    <data-box :id="91" :columns="columns" :data="prdPackageList" @onPageChange="getProductPackage" :page="pageService"></data-box>
     <div class="submitBar">
       <i-row type="flex" align="middle" style="padding:10px">
-        <i-col :span="24" style="text-align:right;">
-          <i-button  @click="pPackageModel=true" class="highButton">新增产品包</i-button>
+        <i-col :span="1" offset="22">
+          <i-button @click="pPackageModel=true" class="highButton">新增产品包</i-button>
         </i-col>
       </i-row>
     </div>
@@ -43,7 +43,9 @@
   import Component from 'vue-class-component';
   import SvgIcon from '~/components/common/svg-icon.vue';
   import AddProductPackage from '~/components/base-data/add-product-package.vue';
-  import { CommonService } from "~/utils/common.service";
+  import {
+    CommonService
+  } from "~/utils/common.service";
   import {
     Dependencies
   } from '~/core/decorator';
@@ -184,7 +186,7 @@
           key: 'remark',
           align: 'center',
           editable: true,
-          ellipsis:true
+          ellipsis: true
         },
       ];
       this.productModel = {
@@ -218,7 +220,7 @@
     QuotationTemplatedownload() {
       this.productPackageService.downloadTemplate().subscribe(
         val => {
-          CommonService.downloadFile(val,'报价模版下载')
+          CommonService.downloadFile(val, '报价模版下载')
         },
         ({
           msg
@@ -281,7 +283,7 @@
         })
         .subscribe(
           val => {
-            CommonService.downloadFile(val,"文件下载")
+            CommonService.downloadFile(val, "文件下载")
             this.$Message.success('下载成功！');
           },
           ({
@@ -304,7 +306,7 @@
     /**
      * 预览
      */
-    preView(row){
+    preView(row) {
       this.productPackageService
         .downloadProductPackage({
           fileId: row.fileId,
@@ -315,8 +317,8 @@
             // CommonService.downloadFile(val,"文件下载")
           },
           ({
-             msg
-           }) => {
+            msg
+          }) => {
             this.$Message.error(msg);
           }
         );
@@ -325,6 +327,32 @@
 
 </script>
 <style lang="less" scoped>
+  .page.prod-package-info {
+    .data-form {
+      margin-top: 10px;
+      margin-left: 10px;
+      .title {
+        margin-right: 10px;
+      }
+      .data-form-item {
+        display: inline-block;
+        width: 10%;
+        margin-right: 10px;
+      }
+      .command {
+        float: right;
+        margin-right: 10px;
+        margin-top: 10px;
+        .command-item {
+          font-size: 16px;
+          cursor: pointer;
+          margin-left: 10px;
+          color: #3367A7
+        }
+      }
+    }
+  }
+  
   .ivu-table-fixed-body {
     height: auto !important;
   }
