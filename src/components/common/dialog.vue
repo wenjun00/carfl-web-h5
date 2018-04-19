eb2f110a2355029c38e2d35814defb3b242fa1ce
zhuchentong <2037630@163.com>
2018-04-19 15:19:22 +0800

    update

---
+++ src/components/common/dialog.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="component dialog">
+    <i-modal>
+
+    </i-modal>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+
+@Component({
+  components: {}
+})
+export default class Dialog extends Vue {
+  
+}
+</script>
+
+<style lang="less" scoped>
+.component.dailog {
+}
+</style>
