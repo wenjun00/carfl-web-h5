f9eaf377fbc9d5c314c2ac877acb0e312fa1b7bd
[sujian] <[744929434@qq.com]>
2018-04-19 16:58:42 +0800

    '提取style'

--- src/components/finance-manage/confirm-pay.vue
+++ src/components/finance-manage/confirm-pay.vue
@@ -11,10 +11,11 @@
         <i-col :span="12">
           <i-form-item label="付款类型" prop="refundType">
             <i-select v-model="repaymentObj.refundType" disabled>
-              <i-option v-for="{value,label} in $dict.getDictData('0430')" :key="value" :label="label" :value="value"></i-option>
+              <i-option v-for="{value,label} in $dict.getDictData('0430')" :key="value" :label="label"
+                        :value="value"></i-option>
             </i-select>
           </i-form-item>
-          <i-button style="float:right;color:#265ea2" type="text" @click="saleApplyInfo">销售申请详情</i-button>
+          <i-button class="modal-item-salebutton" type="text" @click="saleApplyInfo">销售申请详情</i-button>
         </i-col>
       </i-row>
       <i-row>
@@ -31,12 +32,13 @@
       </i-row>
       <i-col :span="24">
         <i-form-item label="备注">
-          <i-input type="textarea" v-model="repaymentObj.remark" style="width:100%" disabled></i-input>
+          <i-input class="modal-item-beizhu-input" type="textarea" v-model="repaymentObj.remark" disabled></i-input>
         </i-form-item>
       </i-col>
     </i-form>
     <div v-if="applicationPhaseResources.length">
-      <div style="width:7px;height:20px;background:#265EA2;display:inline-block;margin-right:6px;position:relative;top:4px;"></div><span>附件</span>
+      <div class="modal-item-fujian-div"></div>
+      <span>附件</span>
     </div>
 
     <div class="invoiceContainer">
@@ -47,9 +49,10 @@
     </div>
 
     <div>
-      <div style="width:7px;height:20px;background:#265EA2;display:inline-block;margin-right:6px;position:relative;top:4px;"></div><span>收款明细</span>
+      <div class="modal-item-shoukuamingxi"></div>
+      <span>收款明细</span>
     </div>
-    <table border="1" width="850" style="margin-top:10px;text-align:center;border:1px solid #DDDEE1">
+    <table class="modal-item-table" border="1" width="850">
       <tr height="40">
         <td bgcolor="#F2F2F2" colspan="1" width="40%">项目</td>
         <td bgcolor="#F2F2F2" colspan="1" width="60%">金额（元）</td>
@@ -60,19 +63,20 @@
       </tr>
       <tr height="40">
         <td>合计</td>
-        <td style="font-weight:700;font-size:14px">{{repaymentObj.refundTotalAmount}}</td>
+        <td class="modal-item-heji-td">{{repaymentObj.refundTotalAmount}}</td>
       </tr>
     </table>
 
     <div>
-      <div style="width:7px;height:20px;background:#265EA2;display:inline-block;margin-right:6px;position:relative;top:4px;"></div><span>收款方式</span>
+      <div class="modal-item-shoukuanfangshi-div"></div>
+      <span>收款方式</span>
     </div>
 
-    <table border="1" width="850" style="margin-top:10px;text-align:center;border:1px solid #DDDEE1">
+    <table class="modal-item-table" border="1" width="850">
       <tr height="40">
-        <td bgcolor="#F2F2F2" colspan="1" width="5%"  v-if="!check">
+        <td bgcolor="#F2F2F2" colspan="1" width="5%" v-if="!check">
           <div @click="addObj">
-            <i-icon type="plus" style="color:#199ED8;cursor:pointer"></i-icon>
+            <i-icon class="modal-item-icon" type="plus"></i-icon>
           </div>
         </td>
         <td bgcolor="#F2F2F2" colspan="1" width="20%">结算通道</td>
@@ -81,56 +85,65 @@
         <td bgcolor="#F2F2F2" colspan="1">状态</td>
       </tr>
       <tr height="40" v-for="(v,i) in collectMoneyDetails" :key="i">
-        <td  v-if="!check">
+        <td v-if="!check">
           <div @click="deleteObj(i)">
-            <i-icon type="minus" style="color:#199ED8;cursor:pointer"></i-icon>
+            <i-icon type="minus" class="modal-item-icon"></i-icon>
           </div>
         </td>
         <td>
-          <i-select placeholder="选择结算通道" style="display:inline-block;width:90%" v-model="v.refundChannel"  :disabled="check">
-            <i-option v-for="{value,label} in $dict.getDictData('0107')" :key="value" :label="label" :value="value"></i-option>
+          <i-select class="modal-item-select" placeholder="选择结算通道" v-model="v.refundChannel"
+                    :disabled="check">
+            <i-option v-for="{value,label} in $dict.getDictData('0107')" :key="value" :label="label"
+                      :value="value"></i-option>
           </i-select>
         </td>
         <td>
-          <i-select placeholder="选择收款项目" style="display:inline-block;width:90%" v-model="v.refundItem" :disabled="check">
-            <i-option v-for="{value,label} in $dict.getDictData('0430')" :key="value" :label="label" :value="value"></i-option>
+          <i-select placeholder="选择收款项目" class="modal-item-select" v-model="v.refundItem"
+                    :disabled="check">
+            <i-option v-for="{value,label} in $dict.getDictData('0430')" :key="value" :label="label"
+                      :value="value"></i-option>
           </i-select>
         </td>
         <td>
-          <i-input style="display:inline-block;width:30%;margin-right:10px" v-model="v.refundAmount" @on-blur="inputBlur" :readonly="check"></i-input>
+          <i-input class="modal-item-huakou-input" v-model="v.refundAmount"
+                   @on-blur="inputBlur" :readonly="check"></i-input>
           <i-button class="blueButton" v-if="!check">确认划扣</i-button>
         </td>
         <td><span>{{$dict.getDictName(v.dealStatus)}}</span>
-          <i-icon type="loop" size="20" color="#199ED8" style="margin-left:6px;cursor:pointer"></i-icon>
+          <i-icon class="modal-item-huakou-icon"type="loop" size="20" color="#199ED8"></i-icon>
         </td>
       </tr>
       <tr height="40">
         <td v-if="!check"></td>
         <td width="25%">合计（元）</td>
-        <td  colspan="3" style="font-weight:700;font-size:14px">{{paymentAmount}}</td>
+        <td class="modal-item-heji-td" colspan="3">{{paymentAmount}}</td>
       </tr>
     </table>
 
     <!--<i-table :columns3="columns3" :data3="data3"></i-table>-->
     <div>
-      <div style="width:7px;height:20px;background:#265EA2;display:inline-block;margin-right:6px;position:relative;top:4px;margin-top:10px;"></div><span>账户信息</span>
+      <div class="modal-item-zhanghuxinxi-div"></div>
+      <span>账户信息</span>
     </div>
     <i-table :columns="columns2" :data="personalBanks"></i-table>
 
     <div v-if="!check||financeUploadResources.length">
-      <div style="width:7px;height:20px;background:#265EA2;display:inline-block;margin-right:6px;position:relative;top:4px;margin-top:10px;"></div><span>收款凭证</span>
+      <div class="modal-item-shoukuanpingzheng-div"></div>
+      <span>收款凭证</span>
     </div>
 
-    <i-row style="margin-top:10px">
-      <i-col :span="12"  v-if="!check">
-        <div style="height:200px;width:200px;border:1px solid #C2C2C2;cursor:pointer;text-align:center;position:relative;left:40px;" @click="openUpload=true">
-          <Icon type="plus-circled" style="display:block;margin-top:53px;" size="40" color="#265ea2"></Icon>
+    <i-row class="modal-item-upload">
+      <i-col :span="12" v-if="!check">
+        <div class="modal-item-upload-div" @click="openUpload=true">
+          <Icon type="plus-circled" class="modal-item-upload-icon" size="40" color="#265ea2"></Icon>
           <div>点击添加附件</div>
-          <span style="color:gray">支持jpg/pdf/png格式建议大小不超过10M</span>
+          <span class="modal-item-upload-text">支持jpg/pdf/png格式建议大小不超过10M</span>
         </div>
       </i-col>
-      <i-col :span="8" v-for="(v,i) in financeUploadResources" :key="v.id" style="display:flex;justify-content:center;margin-top:10px">
-        <div :style="`height:200px;width:200px;border:1px solid #C2C2C2;background-image:url(${v.materialUrl});background-repeat:no-repeat;`">
+      <i-col :span="8" v-for="(v,i) in financeUploadResources" :key="v.id"
+             class="modal-item-upload-col">
+        <div
+          :style="`height:200px;width:200px;border:1px solid #C2C2C2;background-image:url(${v.materialUrl});background-repeat:no-repeat;`">
         </div>
       </i-col>
     </i-row>
@@ -166,7 +179,7 @@
   import {
     RefundApplicationService
   } from "~/services/manage-service/refund-application.service";
-  import { Prop } from "vue-property-decorator";
+  import {Prop} from "vue-property-decorator";
 
   @Component({
     components: {
@@ -199,7 +212,7 @@
 
     private columns2: any;
     private columns3: any;
-    private data3: Array < Object > = [];
+    private data3: Array<Object> = [];
     private purchaseInfoModel: Boolean = false;
     private gatherModal: Object = {
       gatherType: '销售收款',
@@ -220,6 +233,7 @@
         fileUpload.reset();
       });
     }
+
     /**
      * 上传文件
      */
@@ -227,13 +241,14 @@
       let fileUpload: any = this.$refs["file-upload"];
       fileUpload.upload();
     }
+
     refresh(row) {
       this.rowObj = row
       this.refundApplicationService.getRefundApplicationById({
         refundId: row.refundApplicationId
       }).subscribe(data => {
         console.log(data)
-        this.collectMoneyId = data.collectMoneyHistory?data.collectMoneyHistory.id:''
+        this.collectMoneyId = data.collectMoneyHistory ? data.collectMoneyHistory.id : ''
         this.personal = data.personal
         this.productOrder = data.productOrder
         this.itemList = data.itemList
@@ -245,11 +260,12 @@
         this.applicationPhaseResources = data.resourceList.filter(v => v.materialType === 1162)
         this.inputBlur()
       }, ({
-        msg
-      }) => {
+            msg
+          }) => {
         this.$Message.error(msg)
       })
     }
+
     /**
      * 计算总计
      */
@@ -261,12 +277,14 @@
       console.log(sum)
       this.paymentAmount = sum
     }
+
     mounted() {
       let target = document.querySelector(".purchaseInformation .ivu-modal-body")
       if (target) {
         target.addEventListener('scroll', this.monitorScorll)
       }
     }
+
     monitorScorll() {
       let target = document.querySelector(".purchaseInformation .ivu-modal-body")
       if (target) {
@@ -274,6 +292,7 @@
         console.log(this.scrollTopHeight)
       }
     }
+
     /**
      * 增加还款对象
      */
@@ -284,6 +303,7 @@
         collectMoneyMethod: ''
       })
     }
+
     /**
      * 删除还款对象
      */
@@ -292,6 +312,7 @@
       this.collectMoneyDetails.splice(index, 1)
       this.inputBlur()
     }
+
     created() {
       this.columns3 = [{
         align: "center",
@@ -400,9 +421,11 @@
         key: 'clientNumber'
       }]
     }
+
     changeBankCard() {
 
     }
+
     addRow() {
       // let tr: any = document.createElement('tr');
       // let tb: any = document.getElementsByClassName('gather_type_table')[0]
@@ -415,6 +438,7 @@
       // }
       // tb.tBodies[0].appendChild(tr);
     }
+
     saleApplyInfo() {
       this.purchaseInfoModel = true
       let _purchaseInfo: any = this.$refs["purchase-info"];
@@ -424,7 +448,7 @@
 
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
   .invoiceContainer {
     display: flex;
     justify-content: flex-start;
@@ -440,6 +464,76 @@
     }
     .invoiceName {
       text-align: center;
+    }
+  }
+
+  .component.confirm-pay {
+    .modal-item-salebutton {
+      float: right;
+      color: #265ea2
+    }
+    .modal-item-beizhu-input {
+      width: 100%;
+    }
+    .modal-item-fujian-div {
+      width: 7px;
+      height: 20px;
+      background: #265EA2;
+      display: inline-block;
+      margin-right: 6px;
+      position: relative;
+      top: 4px;
+    }
+    .modal-item-shoukuamingxi {
+      width: 7px;
+      height: 20px;
+      background: #265EA2;
+      display: inline-block;
+      margin-right: 6px;
+      position: relative;
+      top: 4px;
+    }
+    .modal-item-table{
+      margin-top:10px;text-align:center;border:1px solid #DDDEE1
+    }
+    .modal-item-heji-td{
+      font-weight:700;font-size:14px
+    }
+    .modal-item-shoukuanfangshi-div{
+      width:7px;height:20px;background:#265EA2;display:inline-block;margin-right:6px;position:relative;top:4px;
+    }
+    .modal-item-icon{
+      color:#199ED8;cursor:pointer
+    }
+    .modal-item-select{
+      display:inline-block;width:90%;
+    }
+    .modal-item-huakou-input{
+      display:inline-block;width:30%;margin-right:10px
+    }
+    .modal-item-huakou-icon{
+      margin-left:6px;cursor:pointer
+    }
+    .modal-item-zhanghuxinxi-div{
+      width:7px;height:20px;background:#265EA2;display:inline-block;margin-right:6px;position:relative;top:4px;margin-top:10px;
+    }
+    .modal-item-shoukuanpingzheng-div{
+      width:7px;height:20px;background:#265EA2;display:inline-block;margin-right:6px;position:relative;top:4px;margin-top:10px;
+    }
+    .modal-item-upload{
+      margin-top:10px;
+      .modal-item-upload-div{
+        height:200px;width:200px;border:1px solid #C2C2C2;cursor:pointer;text-align:center;position:relative;left:40px;
+      }
+      .modal-item-upload-icon{
+        display:block;margin-top:53px;
+      }
+      .modal-item-upload-text{
+        color:gray
+      }
+      .modal-item-upload-col{
+        display:flex;justify-content:center;margin-top:10px
+      }
     }
   }
 
