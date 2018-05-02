c76d9e5b1c6ba797995f6aa7b2c09a26e8cbfe31
chenli <1019706609@qq.com>
2018-05-02 17:54:57 +0800

    modify

--- src/components/purchase-manage/check-bank-card.vue
+++ src/components/purchase-manage/check-bank-card.vue
@@ -12,6 +12,9 @@
     Prop
   } from "vue-property-decorator";
   import DataBox from "~/components/common/data-box.vue";
+  import {
+    CityService
+  } from '~/utils/city.service'
   import CheckAttachment from '~/components/finance-manage/check-attachment.vue'
 
   @Component({
@@ -82,43 +85,64 @@
           key: 'accountType',
           minWidth: this.$common.getColumnWidth(3),
         },
-        {
-          align: 'center',
-          editable: true,
-          title: '开户城市',
-          key: 'depositCity',
-          minWidth: this.$common.getColumnWidth(3),
-        },
+        // {
+        //   align: 'center',
+        //   editable: true,
+        //   title: '开户省份',
+        //   key: 'depositProvince',
+        //   minWidth: this.$common.getColumnWidth(3),
+        //   render: (h, {
+        //     row,
+        //     column,
+        //     index
+        //   }) => {
+        //     return h('span', CityService.getCityName(row.depositProvince))
+        //   }
+        // },
+        // {
+        //   align: 'center',
+        //   editable: true,
+        //   title: '开户城市',
+        //   key: 'depositCity',
+        //   minWidth: this.$common.getColumnWidth(3),
+        //   render: (h, {
+        //     row,
+        //     column,
+        //     index
+        //   }) => {
+        //     return h('span', CityService.getCityName(row.depositCity))
+        //   }
+        // },
         {
           title: '验卡失败原因',
           key: 'cardFailure',
           //   editable: true,
           minWidth: this.$common.getColumnWidth(6),
-        //   renderHeader: (h, {
-        //     column,
-        //     index
-        //   }) => {
-        //     return h(
-        //       'div', {
-        //         on: {
-        //           click: () => {
-        //             this.openHelpPage()
-        //           }
-        //         },
-        //         style: {
-        //           cursor: 'pointer'
-        //         }
-        //       }, [
-        //         h('span', {}, '验卡失败原因'),
-        //         h('Icon', {
-        //           props: {
-        //             type: 'help-circled',
-        //             size: '20'
-        //           }
-        //         })
-        //       ]
-        //     )
-        //   },
+          //   renderHeader: (h, {
+          //     column,
+          //     index
+          //   }) => {
+          //     return h(
+          //       'div', {
+          //         on: {
+          //           click: () => {
+          //             this.openHelpPage()
+          //           }
+          //         },
+          //         style: {
+          //           cursor: 'pointer'
+          //         }
+          //       }, [
+          //         h('span', {}, '验卡失败原因'),
+          //         h('Icon', {
+          //           props: {
+          //             type: 'help-circled',
+          //             size: '20'
+          //           }
+          //         })
+          //       ]
+          //     )
+          //   },
           align: 'center'
         },
         // {
@@ -132,13 +156,6 @@
           editable: true,
           title: '预览手机号',
           key: 'reservedPhoneNumber',
-          minWidth: this.$common.getColumnWidth(3),
-        },
-        {
-          align: 'center',
-          editable: true,
-          title: '省份',
-          key: 'depositProvince',
           minWidth: this.$common.getColumnWidth(3),
         },
         {
