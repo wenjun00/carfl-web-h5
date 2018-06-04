8501baf0da2de20b813c65a9e9fe15f35eaad004
huangrui <hr837@163.com>
2018-06-04 09:49:04 +0800

    修改路由配置

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -9,66 +9,82 @@ const UploadIdPhotoTwo = () => Promise.resolve(require('~/pages/upload-id-photo-
 const UploadIdPhotoThree = () => Promise.resolve(require('~/pages/upload-id-photo-three.vue'))
 const AddInformation = () => Promise.resolve(require('~/pages/add-information.vue'))
 const AddDocumentInfor = () => Promise.resolve(require('~/pages/add-document-infor.vue'))
+import MyOrder from "~/pages/my-order.vue"
+import PaymentRecord from "~/pages/payment-record.vue";
 
 // 路由信息配置
 const routes = [
-    {
-        path: '/home',
-        name: 'Home',
-        component: Home,
-        meta: {
-            title: '洋葱汽车'
-        }
-    }, {
-        path: '/subscribe',
-        name: 'Subscribe',
-        component: Subscribe,
-        meta: {
-            title: '帮我买车'
-        }
-    }, {
-        path: '/FAQ',
-        name: 'FAQ',
-        component: FAQ
-    }, {
-        path: '/buy-car-list',
-        name: 'BuyCarList',
-        component: BuyCarList
-    }, {
-        path: '/custom-information',
-        name: 'custom-information',
-        component: CustomInformation
-    },
-    {
-        path: '/contact-information',
-        name: 'contact-information',
-        component: ContactInformation
-    },
-    {
-        path: '/upload-id-photo-first',
-        name: 'upload-id-photo-first',
-        component: UploadIdPhotoFirst
-    },
-    {
-        path: '/upload-id-photo-two',
-        name: 'upload-id-photo-two',
-        component: UploadIdPhotoTwo
-    },
-    {
-        path: '/upload-id-photo-three',
-        name: 'upload-id-photo-three',
-        component: UploadIdPhotoThree
-    },
-    {
-        path: '/add-information',
-        name: 'add-information',
-        component: AddInformation
-    },
-    {
-        path: '/add-document-infor',
-        name: 'add-document-infor',
-        component: AddDocumentInfor
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home,
+    meta: {
+      title: '洋葱汽车'
     }
+  }, {
+    path: '/subscribe',
+    name: 'Subscribe',
+    component: Subscribe,
+    meta: {
+      title: '帮我买车'
+    }
+  }, {
+    path: '/FAQ',
+    name: 'FAQ',
+    component: FAQ
+  }, {
+    path: '/buy-car-list',
+    name: 'BuyCarList',
+    component: BuyCarList
+  }, {
+    path: '/custom-information',
+    name: 'custom-information',
+    component: CustomInformation
+  },
+  {
+    path: '/contact-information',
+    name: 'contact-information',
+    component: ContactInformation
+  },
+  {
+    path: '/upload-id-photo-first',
+    name: 'upload-id-photo-first',
+    component: UploadIdPhotoFirst
+  },
+  {
+    path: '/upload-id-photo-two',
+    name: 'upload-id-photo-two',
+    component: UploadIdPhotoTwo
+  },
+  {
+    path: '/upload-id-photo-three',
+    name: 'upload-id-photo-three',
+    component: UploadIdPhotoThree
+  },
+  {
+    path: '/add-information',
+    name: 'add-information',
+    component: AddInformation
+  },
+  {
+    path: '/add-document-infor',
+    name: 'add-document-infor',
+    component: AddDocumentInfor
+  }, {
+    path: '/my-order',
+    name: 'MyOrder',
+    meta: {
+      title: '我的订单'
+    },
+    component: MyOrder
+  }, {
+    path: '/payment-record',
+    name: 'PaymentRecord',
+    meta: {
+      title: '还款明细'
+    },
+    component: PaymentRecord
+  }
 ]
 
 export default routes;
