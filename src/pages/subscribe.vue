19e36af62e3cb3abc96f083f3d3f318f8f83097f
baipengfei <782322170@qq.com>
2018-10-30 23:11:34 +0800

    modify

--- src/pages/subscribe.vue
+++ src/pages/subscribe.vue
@@ -6,7 +6,7 @@
       <van-field v-model="model.name" required disabled icon="arrow" placeholder="请选择所在地" @click-icon="$toast('question')"/>
       <van-field v-model="model.huo" required disabled icon="arrow" placeholder="首次上牌时间" @click-icon="$toast('question')"/>
       <van-field v-model="model.gujia" center required placeholder="请输入行驶公里">
-        <p slot="button" size="small"text>万公里</p>
+        <p slot="button" size="small" text >万公里</p>
       </van-field>
     </van-cell-group>
     <van-button type="primary" class="nextStepSty" bottom-action @click="addAffirm">获取估价</van-button>
